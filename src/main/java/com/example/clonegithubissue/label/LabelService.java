3483800a8ba2c31e6c563b1fe3a7eb5772190ff3
mybloom <baejungeun11@gmail.com>
2022-07-22 16:28:31 +0900

    fix: NPE-probable code fix ,apply paging in getListLabel API (#2)
    - fix entity not navigating when data is empty.

--- src/main/java/com/example/clonegithubissue/label/LabelService.java
+++ src/main/java/com/example/clonegithubissue/label/LabelService.java
@@ -3,13 +3,16 @@ package com.example.clonegithubissue.label;
 import com.example.clonegithubissue.common.dto.ApiResourceType;
 import com.example.clonegithubissue.common.dto.ApiResponse;
 import com.example.clonegithubissue.common.dto.RelationDataResponse;
-import com.example.clonegithubissue.common.dto.RootDataResponse;
+import com.example.clonegithubissue.common.dto.ResourceDataResponse;
 import com.example.clonegithubissue.label.dto.LabelListResponse;
 import com.example.clonegithubissue.member.Member;
 import com.example.clonegithubissue.member.dto.MemberDetailResponse;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
@@ -18,10 +21,12 @@ public class LabelService {
 
 	private final LabelRepository labelRepository;
 
-	public ApiResponse retrieveList(Long memberId) {
-		List<Label> labels = labelRepository.findByAuthor(Member.of(memberId));
+	public ApiResponse retrieveList(Long memberId, Integer page, Integer size) {
 
-		RootDataResponse<LabelListResponse> dataResponse = new RootDataResponse<>();
+		PageRequest pageRequest = PageRequest.of(page, size);
+		Page<Label> labels = labelRepository.findByAuthor(Member.of(memberId), pageRequest);
+
+		ResourceDataResponse<LabelListResponse> dataResponse = new ResourceDataResponse<>();
 		dataResponse.setType(ApiResourceType.LABEL.getResourceType());
 		dataResponse.setAttributes(labels.stream()
 			.map(label -> new LabelListResponse(label.getId(), label.getTitle(), label.getColor()))
@@ -29,8 +34,18 @@ public class LabelService {
 
 		RelationDataResponse<MemberDetailResponse> relationResponse = new RelationDataResponse<>();
 		relationResponse.setType(ApiResourceType.MEMBER.getResourceType());
-		Member author = labels.get(0).getAuthor();
-		relationResponse.setAttributes(List.of(new MemberDetailResponse(author.getOauthId(),author.getOauthName())));
+
+		Member author = null;
+		if (!labels.getContent().isEmpty()) {
+			author = labels.getContent().get(0).getAuthor();
+		}
+
+		if(author == null) {
+			relationResponse.setAttributes(Collections.emptyList());
+		}else{
+			relationResponse.setAttributes(
+			List.of(new MemberDetailResponse(author.getOauthId(), author.getOauthName())));
+		}
 
 		dataResponse.setRelationships(relationResponse);
 
