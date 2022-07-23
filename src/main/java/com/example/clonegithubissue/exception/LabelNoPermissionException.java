03511a749ca4912783c863569822cc56bc621e02
mybloom <baejungeun11@gmail.com>
2022-07-23 15:10:05 +0900

    refactor: change error message in LabelNoPermissionException (#2)

--- src/main/java/com/example/clonegithubissue/exception/LabelNoPermissionException.java
+++ src/main/java/com/example/clonegithubissue/exception/LabelNoPermissionException.java
@@ -4,6 +4,6 @@ public class LabelNoPermissionException extends NotFoundException{
 
 	@Override
 	protected String getBodyMessage() {
-		return "There is no permission to edit label of the labelId.";
+		return "There is no permission to edit/delete label of the labelId.";
 	}
 }
