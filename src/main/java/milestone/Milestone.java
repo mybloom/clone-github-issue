6dab696b13bbf4d817163f94e670d1da6e348b68
mybloom <baejungeun11@gmail.com>
2022-07-24 22:07:21 +0900

    feat: add Issue entity bidirectional association to Milestone entity (#3)
    - when searching for milestone, issues are always searched, so bidirectional association is set in Milestone Entity.

--- src/main/java/milestone/Milestone.java
+++ src/main/java/milestone/Milestone.java
@@ -1,11 +1,16 @@
 package milestone;
 
+import com.example.clonegithubissue.issue.Issue;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -27,6 +32,9 @@ public class Milestone {
 	private String description;
 
 	private LocalDate completionDate;
+
+	@OneToMany(mappedBy = "milestone")
+	private Set<Issue> issues = new HashSet<>();
 
 	@Column(name = "member_id")
 	private Long authorId;
