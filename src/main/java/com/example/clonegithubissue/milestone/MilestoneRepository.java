d7cbac0954bf0591ff2a4e656307fcff0cc6b94c
mybloom <baejungeun11@gmail.com>
2022-07-25 13:49:31 +0900

    feat: Add query method findById() in MilestoneRepository  (#3)

--- src/main/java/com/example/clonegithubissue/milestone/MilestoneRepository.java
+++ src/main/java/com/example/clonegithubissue/milestone/MilestoneRepository.java
@@ -1,5 +1,6 @@
 package com.example.clonegithubissue.milestone;
 
+import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
@@ -10,4 +11,7 @@ public interface MilestoneRepository extends JpaRepository<Milestone, Long> {
 	@EntityGraph(attributePaths = {"issues"})
 	Page<Milestone> findByAuthorId(Long authorId, Pageable pageable);
 
+	@EntityGraph(attributePaths = {"issues"})
+	@Override
+	Optional<Milestone> findById(Long milestoneId);
 }
