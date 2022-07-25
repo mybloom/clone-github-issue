18108374466584ca2ad3f38aa799d19ecfecbc92
mybloom <baejungeun11@gmail.com>
2022-07-25 13:30:29 +0900

    feat: Add method findByAuthorId() to the MilestoneRepository  (#3)
    - for getList by authorId

--- src/main/java/com/example/clonegithubissue/milestone/MilestoneRepository.java
+++ src/main/java/com/example/clonegithubissue/milestone/MilestoneRepository.java
@@ -1,8 +1,13 @@
 package com.example.clonegithubissue.milestone;
 
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MilestoneRepository extends JpaRepository<Milestone, Long> {
+
+	@EntityGraph(attributePaths = {"issues"})
+	Page<Milestone> findByAuthorId(Long authorId, Pageable pageable);
 
 }
